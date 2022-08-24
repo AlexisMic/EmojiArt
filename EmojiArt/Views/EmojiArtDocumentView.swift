ec1d9c94b239ae2d8ccbc2cab3be35087e070fda
AlexisMic <amschotte@me.com>
2022-08-24 13:07:51 +0100

    still working on View

--- EmojiArt/Views/EmojiArtDocumentView.swift
+++ EmojiArt/Views/EmojiArtDocumentView.swift
@@ -12,10 +12,50 @@ struct EmojiArtDocumentView: View {
     @ObservedObject var emojiArtDocument: EmojiArtDocument
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 0) {
+            documentBody
+            pallete
+        }
+        
     }
+    
+    private var documentBody: some View {
+        Color.yellow
+    }
+    
+    private var pallete: some View {
+        ScrollingViewEmojis(emojis: emojisTest)
+    }
+    
+ 
+    var emojisTest = "😆😍🐨🙉🐣🍈🍅🥒🎾🥋🏂🚗🚔🚟🖥📱⏰🔆🅿️🇧🇷🇺🇸🇪🇺"
+    
 }
+
+struct ScrollingViewEmojis: View {
+    
+    let emojis: String
+    
+    var body: some View {
+        ScrollView(.horizontal) {
+            HStack {
+                ForEach(emojis.map({String($0)}), id:\.self) { emoji in
+                    Text(emoji)
+                }
+            }
+        }
+    }
+    
+    
+}
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
