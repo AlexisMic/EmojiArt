25bc5242ec677731de89efeb2c5dd7c11015fd19
AlexisMic <amschotte@me.com>
2022-08-24 11:20:05 +0100

    EmojiArtModel

---
+++ EmojiArt/EmojiArtModel.swift
@@ -0,0 +1,42 @@
+//
+//  EmojiArtModel.swift
+//  EmojiArt
+//
+//  Created by Alexis Schotte on 8/23/22.
+//
+
+import Foundation
+
+struct EmojiArtModel {
+    var background = Background.blank
+    var emojis: [Emoji] = []
+    private var uniqueEmojiId = 0
+    
+    mutating func addEmoji(text: String, at position: (x: Int, y: Int), size: Int) {
+        uniqueEmojiId += 1
+        emojis.append(Emoji(id: uniqueEmojiId, text: text, at: (x: position.x, y: position.y), size: size))
+    }
+    
+    init() { }
+    
+    struct Emoji {
+        let id: Int
+        let text: String
+        var x: Int
+        var y: Int
+        var size: Int
+        
+        fileprivate init(id: Int, text: String, at position: (x: Int, y: Int), size: Int) {
+            self.id = id
+            self.text = text
+            self.x = position.x
+            self.y = position.y
+            self.size = size
+        }
+        
+    }
+    
+    enum Background {
+    case blank, URL, Image
+    }
+}
